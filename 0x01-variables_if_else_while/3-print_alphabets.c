d47df0c5255f2b662d4b08ca94d4838d5a652a80
CKMueni <ckaymueni@gmail.com>
2022-09-12 16:23:56 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char upper;
+	char lower;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		putchar(lower);
+	}
+	for (upper = 'A'; upper <= 'Z'; upper++)
+	{
+		putchar(upper);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
