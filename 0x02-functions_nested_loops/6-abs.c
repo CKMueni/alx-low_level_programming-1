ea3e9b1962d27195a2777e87c4fec455f4a8d12d
CKMueni <ckaymueni@gmail.com>
2022-09-17 17:29:22 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *  _abs - computes absolute value of an integer
+ *  @n: check for parameter
+ *
+ * Return: Always 0 (Success)
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = -n;
+		return (n)
+	}
+
+	return (n);
+}
