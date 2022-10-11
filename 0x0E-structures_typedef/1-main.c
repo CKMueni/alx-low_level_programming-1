6de59b57259eada37ad1de3e4554cdc2c23b4897
CKMueni <ckaymueni@gmail.com>
2022-10-11 03:22:07 -0700

    Task 0

---
+++ 0x0E-structures_typedef/1-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - check the code for alx students
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	struct dog my_dog;
+
+	init_dog(%mydog, "poppy", 3.5, "Bob");
+	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+	return (0);
+}
+
+
+
