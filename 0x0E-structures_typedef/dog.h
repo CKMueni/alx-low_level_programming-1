2236749b8c7942f0789112130cda5f9e127f4562
CKMueni <ckaymueni@gmail.com>
2022-10-11 03:40:27 -0700

    Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,15 +7,22 @@
  * @age: dog age
  * @owner: owner
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog-t;
+};
+
+/**
+ * dog-t - typedef for struct dog
+ */
+
+typedef struct dog dog_t:
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog-t *new_dog(char *name, float age, char *owner);
 void free-dog(dog_t *d);
+
 #endif
